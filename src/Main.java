e6ccda621fe88eb441f51528140583fba7060406
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2024-11-25 14:39:46 +0530

    The Player then checks for a Option.They are No Play,snake or ladder

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,25 @@ public class Main {
         System.out.println("Single Player starts at position " + position);
 
         Random random = new Random();
-        int dice = random.nextInt(6) + 1; // Random number between 1 and 6
+        int dice = random.nextInt(6) + 1;// Random number between 1 and 6
+        int option = random.nextInt(3); // 0 = No Play, 1 = Ladder, 2 = Snake
         System.out.println("Player rolls the die: " + dice);
+
+        switch(option){
+
+            case 0:
+                System.out.println("No Play! Stay at position " + position);
+                break;
+            case 1:
+                position += dice;
+                System.out.println("Ladder! Move to position " + position);
+                break;
+            case 2:
+                position -= dice;
+                if (position < 0) position = 0;
+                System.out.println("Snake! Move to position " + position);
+                break;
+        }
 
     }
 }
