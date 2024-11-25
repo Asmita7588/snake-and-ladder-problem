75854be3c92ba4f5a907c52c9163ddfdbd81b964
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2024-11-25 14:31:43 +0530

    The Player rolls the die to get a number

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         int position = 0;
         System.out.println("Single Player starts at position " + position);
+
+        Random random = new Random();
+        int dice = random.nextInt(6) + 1; // Random number between 1 and 6
+        System.out.println("Player rolls the die: " + dice);
+
     }
 }
